2f568565a2a216cc856b6dcf527c753c36a12134
jerry960331 <jerry960331@gmail.com>
2022-04-22 21:03:34 +0800

    Added some comments

---
+++ image_processor.py
@@ -0,0 +1,192 @@
+import subprocess
+
+from PIL import Image, ImageFilter
+import math
+import logging
+from pathlib import Path
+
+
+def blur_image_edge(path: str = None,
+                    image: Image = None,
+                    color=(255, 255, 255),
+                    save: bool = False):
+    """
+    Source <https://gist.github.com/namieluss/2a57539df54158e0c1575903a988bbb2>
+
+    :param color: What color to use for the edge.  Default is white.
+       If given, this should be a single integer or floating point value
+       for single-band modes, and a tuple for multi-band modes (one value
+       per band).  When creating RGB images, you can also use color
+       strings as supported by the ImageColor module.  If the color is
+       None, the image is not initialised.
+    """
+
+    # blur radius and diameter
+    radius, diameter = 20, 40
+
+    if image is None:
+        # open an image
+        img = Image.open(path)
+    else:
+        img = image
+
+    # Paste image on white background
+    background_size = (img.size[0] + diameter, img.size[1] + diameter)
+    background = Image.new('RGB', background_size, color)
+    background.paste(img, (radius, radius))
+
+    # create new images with white and black
+    mask_size = (img.size[0] + diameter, img.size[1] + diameter)
+    mask = Image.new('L', mask_size, 255)
+
+    black_size = (img.size[0] - diameter, img.size[1] - diameter)
+    black = Image.new('L', black_size, 0)
+
+    # create blur mask
+    mask.paste(black, (diameter, diameter))
+
+    # Blur image and paste blurred edge according to mask
+    blur = background.filter(ImageFilter.GaussianBlur(radius / 2))
+    background.paste(blur, mask=mask)
+
+    if isinstance(image.filename, str):
+        name = image.filename
+    else:
+        name = path
+
+    if save:
+        background.save(f"{Path(name).stem}_edgy_blurred{Path(name).suffix}", quality=100)
+
+    background.filename = f"{Path(name).stem}_edgy_blurred{Path(name).suffix}"
+    logging.info(background.filename)
+    return background
+
+
+def blur_image(path: str = None, image: Image = None, blur_radius: int = 16, save: bool = False):
+    """Blur the image"""
+
+    if image is None:
+        original_image = Image.open(path)
+    else:
+        original_image = image
+    blurred_image = original_image.filter(ImageFilter.GaussianBlur(radius=blur_radius))
+
+    if isinstance(image.filename, str):
+        name = image.filename
+    else:
+        name = path
+
+    if save:
+        blurred_image.save(f"{Path(name).stem}_blurred.png", quality=100)
+
+    blurred_image.filename = f"{Path(name).stem}_blurred.png"
+    logging.info(blurred_image.filename)
+    return blurred_image
+
+
+def crop_image(path: str = None, image: Image = None, save: bool = False):
+    """crop image into 16:9 ratio"""
+
+    logging.info("cropping image")
+
+    if image is None:
+        # open an image
+        image = Image.open(path)
+    else:
+        image = image
+
+    if isinstance(image.filename, str):
+        name = image.filename
+    else:
+        name = path
+
+    width, height = image.size
+    ratio = width / height
+
+    # ratio from 1.76 to 1.78 are acceptable
+    if math.isclose(ratio, 1.77, rel_tol=1e-3):
+        return image
+
+    if ratio > 1.77:
+        wk = height * 1.77
+        offset = (width - wk) / 2
+        print("ratio > 1.77")
+        # (left, upper, right, lower)
+        image = image.crop((int(offset), 0, width - int(offset), height))
+    else:
+        hk = width / 1.77
+        print("ratio <= 1.77")
+        image = image.crop((0, int((height - hk) / 2), width, height - int((height - hk) / 2)))
+
+    if save:
+        image.save(f"{Path(name).stem}_cropped{Path(name).suffix}", quality=100)
+
+    image.filename = f"{Path(name).stem}_cropped{Path(name).suffix}"
+    logging.info(image.filename)
+    return image
+
+
+def mix_image(name: str, foreground: Image, background: Image, save: bool = False):
+    """
+    Mixing image with background image and foreground image.
+    Background image has to be bigger than the foreground
+    """
+    ratio = foreground.width / foreground.height
+    if ratio > 1.77:
+        h = foreground.height // (foreground.width / background.width)
+        foreground = foreground.resize((background.width, int(h)))
+        offset = (background.height - foreground.height) // 2
+        background.paste(foreground, (0, offset))
+    else:
+        w = foreground.width // (foreground.height / background.height)
+        foreground = foreground.resize((int(w), background.height))
+        offset = (background.width - foreground.width) // 2
+        background.paste(foreground, (offset, 0))
+    logging.info(f"image_mix: offset = {offset}")
+    if save:
+        background.save(f"{Path(name).stem}_mixed.png", quality=100)
+    background.filename = f"{Path(name).stem}_mixed.png"
+    logging.info(background.filename)
+    return background
+
+
+def run_waifu_2x(image_path, waifu2x_path, scale_size: int = None):
+    """
+    Run waifu_2x with waifu2x-ncnn-vulkan.
+    link <https://github.com/nihui/waifu2x-ncnn-vulkan>
+
+    :param waifu2x_path:  Path to your waifu_2x
+    :param image_path:
+    :param scale_size: upscale ratio for waifu_2x
+    (1/2/4/8/16/32, default: do a calculation to make image larger than 4K(3840x2160))
+    :type scale_size: int
+    :return: Path for the output image
+    :rtype: str
+    """
+    print(str(Path(__file__).parent) + "/" + image_path)
+
+    if scale_size is None:
+        scale_size = calculate_scale_level(image_path)
+
+    abs_path = str(Path(__file__).parent) + "/" + image_path
+    subprocess.call([waifu2x_path,
+                     "-i",
+                     abs_path,
+                     "-o",
+                     Path(abs_path).stem + "_scaled" + Path(abs_path).suffix,
+                     "-s",
+                     str(scale_size),
+                     "-g",
+                     "0"])
+    return Path(abs_path).stem + "_scaled" + Path(abs_path).suffix
+
+
+def calculate_scale_level(path):
+    image = Image.open(path)
+    width, height = image.size
+    scale_width = 3840 / width
+    scale_height = 2160 / height
+    scale_raw = max(scale_height, scale_width)
+    scale_size = int(math.pow(2, math.ceil(math.log(scale_raw, 2))))
+    logging.info(f"scale level {scale_size}")
+    return scale_size
---
+++ main.py
@@ -0,0 +1,50 @@
+import image_processor
+import logging
+from pathlib import Path
+from PIL import Image
+
+logging.basicConfig(level=logging.INFO)
+
+
+def image_process(path):
+    """
+    An example function to manipulate image process functions
+
+    My own flow:
+    run_waifu_2x -> crop_image -> image_blur -> image_mix -> image_edgy_blur
+    """
+
+    # To test is scaled file exists.
+    # if so, skip it
+    scaled_path = Path(path).stem + "_scaled" + Path(path).suffix
+    scaled_path__ = Path(scaled_path)
+    if not scaled_path__.is_file():
+        waifu2x_path = "/home/oud/Documents/waifu2x-ncnn-vulkan-20210521-ubuntu/waifu2x-ncnn-vulkan"
+        scaled_path = image_processor.run_waifu_2x(image_path=path, waifu2x_path=waifu2x_path)
+    else:
+        logging.info("waifu skipped")
+
+    # maybe something wrong with waifu_2x
+    if not scaled_path__.is_file():
+        logging.error("Scale failed")
+        return
+
+    cropped_image = image_processor.crop_image(scaled_path, save=True)
+    blurred_image = image_processor.blur_image(image=cropped_image, blur_radius=24)
+    foreground = Image.open(scaled_path)
+    # foreground = blur.image_edgy_blur(image=foreground)
+    mix = image_processor.mix_image(name=foreground.filename, foreground=foreground, background=blurred_image, save=False)
+    image_processor.blur_image_edge(path, mix, save=True)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    # img_path = "93131590_p1.png"
+    # image_process(img_path)
+
+    background = Image.open("97640452_p1_scaled.png")
+    foreground = Image.open("97628614_p0.jpg")
+    image_processor.mix_image("test.png", foreground, background, save=True)
+
+    # blur.image_edgy_blur(img_path, save=True)
+
